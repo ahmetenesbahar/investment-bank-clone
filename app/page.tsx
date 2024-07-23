caa8f88faeca082b4167ad15649c6358807ba5be
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-24 01:28:57 +0300

    refactor: update language attribute to Turkish in layout component

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 import React from "react";
 import { NextPage } from "next";
-import { StyledButton } from "@/styles/styles";
 
 const Home: NextPage = () => {
   const handleClick = () => {
     alert("Button clicked!");
   };
 
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <StyledButton onClick={handleClick}>Click Me</StyledButton>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
