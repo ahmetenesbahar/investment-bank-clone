166e8d623149553a4d46d8fbb661602c36622070
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-23 12:09:24 +0300

    chore: add styled-components and react-hook-form dependencies

---
+++ styles/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  background-color: #0070f3;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #005bb5;
+  }
+`;
