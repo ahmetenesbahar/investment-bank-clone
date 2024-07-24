5b05226c0171c5e06ed8f16eb222159349a2c749
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-24 11:14:38 +0300

    refactor: update LoginForm component with SwitchButton and HelpBox

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,27 +1,46 @@
-import React from "react";
-import { FlexColumn, InputLogin, Flex, Form } from "@/styles/styles";
+import React, { useState } from "react";
+import {
+  FlexColumn,
+  InputLogin,
+  Flex,
+  Form,
+  HelpBox,
+  HelpImage,
+} from "@/styles/styles";
 
 import SwitchButton from "./shared/SwitchButton";
 
 const LoginForm: React.FC = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+  };
+
   return (
     <Form>
       <FlexColumn>
-        <Flex width="100%" alignItems="center">
+        <Flex alignItems="center" width="100%" gap="5px">
           <InputLogin
             type="number"
             placeholder="Müşteri Numaranız / TCKN / YKN"
           />
-          <SwitchButton checked />
+          <SwitchButton checked={isChecked} onChange={handleOnChange} />
+          <HelpBox>
+            <HelpImage src="https://www.isbank.com.tr/Internet/Omni/assets/img/information_icon.png" />
+          </HelpBox>
         </Flex>
-        <Flex width="100%">
+        <Flex width="100%" alignItems="center" gap="5px">
           <InputLogin
             type="password"
             pattern="[0-9]*"
-            disabled
             inputMode="numeric"
+            noHover
             placeholder="Şifre / Geçiçi Şifre"
           />
+          <HelpBox>
+            <HelpImage src="https://www.isbank.com.tr/Internet/Omni/assets/img/information_icon.png" />
+          </HelpBox>
         </Flex>
       </FlexColumn>
     </Form>
