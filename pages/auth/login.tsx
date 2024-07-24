311ec9954fe6ad912946474b710f6a7c61d5c6e6
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-24 18:39:05 +0300

    refactor: Update login page to use QueryClientProvider for React Query integration

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,18 +1,23 @@
 import { NextPage } from "next";
 import "@/app/globals.css";
-import { Container, FlexColumn, Flex } from "@/styles/styles";
+import { Container, Flex } from "@/styles/styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import LoginSidebar from "@/components/LoginSidebar";
 import LoginMain from "@/components/LoginMain";
 
+const queryClient = new QueryClient();
+
 const LoginPage: NextPage = () => {
   return (
-    <Container>
-      <Flex>
-        <LoginSidebar />
-        <LoginMain />
-      </Flex>
-    </Container>
+    <QueryClientProvider client={queryClient}>
+      <Container>
+        <Flex>
+          <LoginSidebar />
+          <LoginMain />
+        </Flex>
+      </Container>
+    </QueryClientProvider>
   );
 };
 
