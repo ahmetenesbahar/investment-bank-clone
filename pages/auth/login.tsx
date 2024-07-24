98cc223a5a8ddf6cfe1250def366ded5781ddaef
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-24 13:13:03 +0300

    refactor: Update login page layout with responsive design

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import "@/app/globals.css";
-import { Container, Flex } from "@/styles/styles";
+import { Container, FlexColumn, Flex } from "@/styles/styles";
 
 import LoginSidebar from "@/components/LoginSidebar";
 import LoginMain from "@/components/LoginMain";
@@ -8,8 +8,10 @@ import LoginMain from "@/components/LoginMain";
 const LoginPage: NextPage = () => {
   return (
     <Container>
-      <LoginSidebar />
-      <LoginMain />
+      <Flex>
+        <LoginSidebar />
+        <LoginMain />
+      </Flex>
     </Container>
   );
 };
