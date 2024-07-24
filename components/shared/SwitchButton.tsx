cef9e3600d2a1520731803e46311c14aa1270cbb
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-24 11:14:47 +0300

    refactor: update SwitchButton component with labels and onChange event handler

--- components/shared/SwitchButton.tsx
+++ components/shared/SwitchButton.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { SwitchContainer, SwitchInput, SwitchSlider } from "@/styles/styles";
+import {
+  SwitchContainer,
+  SwitchInput,
+  SwitchSlider,
+  SwitchLabel,
+  SwitchLabelLeft,
+} from "@/styles/styles";
 
 interface SwitchButtonProps {
   checked: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const SwitchButton: React.FC<SwitchButtonProps> = ({ checked }) => {
+const SwitchButton: React.FC<SwitchButtonProps> = ({ checked, onChange }) => {
   return (
     <SwitchContainer>
-      <SwitchInput type="checkbox" checked={checked} />
+      <SwitchInput type="checkbox" checked={checked} onChange={onChange} />
       <SwitchSlider checked={checked} />
+      <SwitchLabel checked={checked}>Hatırla</SwitchLabel>
+      <SwitchLabelLeft checked={!checked}>Hatırla</SwitchLabelLeft>
     </SwitchContainer>
   );
 };
