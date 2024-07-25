f811e6f66ad9af8d1886090041f06d92412612b0
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-25 18:03:28 +0300

    feat: Add Keyboard component for improved user input experience

---
+++ components/shared/Keyboard.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const KeyboardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 203px;
+  height: 184px;
+  margin: 0 auto;
+  gap: 1px;
+  background-color: #fff;
+  padding: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+`;
+
+const Key = styled.button`
+  width: 60px;
+  height: 40px;
+  font-size: 12px;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  background-color: #adb5bd;
+  font-weight: 600;
+  font-size: 14px;
+  color: #fff;
+`;
+
+const SpecialKey = styled(Key)`
+  background-color: #49a5e0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface KeyboardProps {
+  onKeyPress: (key: string) => void;
+}
+
+const Keyboard: React.FC<KeyboardProps> = ({ onKeyPress }) => {
+  const [keys, setKeys] = useState([
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ]);
+
+  const handleBackspace = () => {
+    console.log("Backspace pressed");
+    onKeyPress("backspace");
+  };
+
+  const handleShuffle = () => {
+    console.log("Shuffle pressed");
+    const shuffledKeys = [...keys].sort(() => Math.random() - 0.5);
+    setKeys(shuffledKeys);
+  };
+
+  return (
+    <KeyboardContainer>
+      {keys.slice(1, 10).map((key) => (
+        <Key key={key} onClick={() => onKeyPress(key)}>
+          {key}
+        </Key>
+      ))}
+      <SpecialKey onClick={handleShuffle}>
+        <img src="/assets/numpad_random.png" alt="shuffle" />
+      </SpecialKey>
+      {keys.slice(0, 1).map((key) => (
+        <Key key={key} onClick={() => onKeyPress(key)}>
+          {key}
+        </Key>
+      ))}
+      <SpecialKey onClick={handleBackspace}>
+        <img src="/assets/numpad_delete.png" alt="delete" />
+      </SpecialKey>
+    </KeyboardContainer>
+  );
+};
+
+export default Keyboard;
