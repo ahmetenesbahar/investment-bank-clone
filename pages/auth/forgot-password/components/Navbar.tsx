00e0d3c93c0a6d7c873b0cade90c651e799a3355
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-30 14:58:34 +0300

    chore: update Navbar component with responsive layout and link styling

--- pages/auth/forgot-password/components/Navbar.tsx
+++ pages/auth/forgot-password/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, FlexColumn, Text } from "@/styles/styles";
+import { Flex, FlexColumn, Text, Link } from "@/styles/styles";
 
 const Navbar: React.FC = () => {
   return (
@@ -8,16 +8,17 @@ const Navbar: React.FC = () => {
       alignItems="center"
       width="840px"
       padding="0.5rem 0"
+      responsiveFull
     >
-      <a href="https://www.isbank.com.tr/">
+      <Link href="https://www.isbank.com.tr/" margin="0 0 0 25px ">
         <img src="/assets/isbank_logo.png" alt="" />
-      </a>
+      </Link>
 
       <FlexColumn
         alignItems="center"
         padding="1rem 0 0 0 0"
         gap="3px"
-        margin="0 50px 0  0"
+        margin="0 25px 0  0"
       >
         <img src="/assets/lock.png" alt="" />
         <Text color="#00428a" fontSize="11px" fontWeight="500">
