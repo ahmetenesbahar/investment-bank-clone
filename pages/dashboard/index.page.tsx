aeeb92827c7210040c555460bcc7eab0b9d198de
ahmetenesbahar <enesbahar2303@gmail.com>
2024-08-01 18:19:03 +0300

    feat: add TabProvider to dashboard page

--- pages/dashboard/index.page.tsx
+++ pages/dashboard/index.page.tsx
@@ -4,16 +4,18 @@ import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 import { Container, Flex } from "@/styles/styles";
-
+import { TabProvider } from "./context/TabContext";
 const Dashboard: NextPage = () => {
   return (
-    <Container backgroundColor="#f2f2f2">
-      <Navbar />
-      <Flex>
-        <Sidebar />
-        <Main />
-      </Flex>
-    </Container>
+    <TabProvider>
+      <Container backgroundColor="#f2f2f2">
+        <Navbar />
+        <Flex>
+          <Sidebar />
+          <Main />
+        </Flex>
+      </Container>
+    </TabProvider>
   );
 };
 
