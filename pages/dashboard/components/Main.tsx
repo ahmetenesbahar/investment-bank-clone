c8dc3f661c341f3370d8b53c406e16eeae63c90b
ahmetenesbahar <enesbahar2303@gmail.com>
2024-08-01 18:19:24 +0300

    feat: update Main component to include DashboardLeft and DashboardRight components

--- pages/dashboard/components/Main.tsx
+++ pages/dashboard/components/Main.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import DashboardLeft from "./DashboardLeft";
+import DashboardRight from "./DashboardRight";
+import { Flex } from "@/styles/styles";
 
 const Main: React.FC = () => {
-  return <div>Main</div>;
+  return (
+    <Flex
+      backgroundColor="#fff"
+      width="960px"
+      height="632px"
+      margin="40px 10px"
+    >
+      <DashboardLeft />
+      <DashboardRight />
+    </Flex>
+  );
 };
 
 export default Main;
