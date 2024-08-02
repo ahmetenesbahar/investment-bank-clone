5823b5d82ff38bdf2a9d7aeb1713deff0cc675fb
ahmetenesbahar <enesbahar2303@gmail.com>
2024-08-02 17:39:29 +0300

    feat: add EditAccounts component to dashboard page

--- pages/dashboard/components/DashboardLeft.tsx
+++ pages/dashboard/components/DashboardLeft.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Flex } from "@/styles/styles";
 import LeftHeader from "./LeftHeader";
+import Balance from "./Balance";
+import EditAccounts from "./EditAccounts";
+import { usePage } from "../context/PageContext";
 
 const DashboardLeft: React.FC = () => {
+  const { page } = usePage();
   return (
-    <Flex borderRight="1px solid #e5e5e5 " width="620px" padding="0 20px 0 0">
+    <Flex
+      borderRight="1px solid #e5e5e5 "
+      width="620px"
+      padding="0 20px 0 0"
+      flexDirection="column"
+    >
       <LeftHeader />
+      {page === "editAccounts" ? <EditAccounts /> : <Balance />}
     </Flex>
   );
 };
