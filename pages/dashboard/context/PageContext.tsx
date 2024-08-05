dad17de8cd3bbdd24f5db12fd767528faba2d423
ahmetenesbahar <enesbahar2303@gmail.com>
2024-08-05 10:21:21 +0300

    feat: add menu functionality to PageProvider in dashboard page

--- pages/dashboard/context/PageContext.tsx
+++ pages/dashboard/context/PageContext.tsx
@@ -3,6 +3,8 @@ import React, { createContext, useContext, useState, ReactNode } from "react";
 const PageContext = createContext({
   page: "",
   handlePageChange: (PageValue: string) => {},
+  menu: false,
+  handleOpenMenu: () => {},
 });
 
 interface PageProviderProps {
@@ -13,13 +15,19 @@ export const usePage = () => useContext(PageContext);
 
 export const PageProvider: React.FC<PageProviderProps> = ({ children }) => {
   const [page, setPage] = useState("");
+  const [menu, setMenu] = useState(false);
 
   const handlePageChange = (pageValue: string) => {
     setPage(pageValue);
   };
+  const handleOpenMenu = () => {
+    setMenu(!menu);
+  };
 
   return (
-    <PageContext.Provider value={{ page, handlePageChange }}>
+    <PageContext.Provider
+      value={{ page, handlePageChange, menu, handleOpenMenu }}
+    >
       {children}
     </PageContext.Provider>
   );
