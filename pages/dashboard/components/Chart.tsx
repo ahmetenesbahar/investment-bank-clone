b83768c98c08869eec5bec8befec4bfc437a1127
ahmetenesbahar <enesbahar2303@gmail.com>
2024-08-05 18:48:35 +0300

    refactor: improve Chart component UI and add chartText prop

--- pages/dashboard/components/Chart.tsx
+++ pages/dashboard/components/Chart.tsx
@@ -1,40 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import { PieChart } from "@mui/x-charts/PieChart";
-import { Flex, VerticalLine } from "@/styles/styles";
+import { Flex, VerticalLine, Text } from "@/styles/styles";
+import { usePage } from "../context/PageContext";
 
 interface ChartProps {
   labels: string[];
   backgroundColor: string[];
   data: { label: string; value: number }[];
+  chartText?: string;
 }
 
-const Chart: React.FC<ChartProps> = ({ labels, backgroundColor, data }) => {
+const Chart: React.FC<ChartProps> = ({ backgroundColor, data, chartText }) => {
+  const { hideNumbers } = usePage();
+
+  const totalValue = data.reduce((acc, curr) => acc + curr.value, 0);
+  const chartLabels = data.map((item) => item.label);
+  const isValueZero = totalValue === 0;
+  const newData = isValueZero
+    ? [{ ...data[0], value: 0.1 }, ...data.slice(1)]
+    : data;
+
   return (
-    <Flex width="100%" position="relative">
+    <Flex margin="0px  0 0 -60px">
       <Flex
-        backgroundColor="#fff"
-        width="4px"
-        height="20px"
-        position="absolute"
-        left="150px"
-        top="10%"
-        zIndex="999"
-      />
-      <PieChart
-        series={[
-          {
-            paddingAngle: 5,
-            innerRadius: 60,
-            outerRadius: 80,
-            data,
-          },
-        ]}
-        width={400}
-        height={200}
-        slotProps={{
-          legend: { itemMarkHeight: 12, itemMarkWidth: 12 },
-        }}
-      />
+        width="100%"
+        position="relative"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Flex
+          backgroundColor="#fff"
+          width="4px"
+          height="20px"
+          position="absolute"
+          left="150px"
+          top="10%"
+          zIndex="999"
+        />
+        <Flex
+          flexDirection="column"
+          position="absolute"
+          left="120px"
+          top="38%"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text color="#000" fontWeight="500" fontSize="12px">
+            {chartText}
+          </Text>
+          <Text fontWeight="700" color="#000" fontSize="16px">
+            {!hideNumbers && totalValue}
+          </Text>
+          <Text textAlign="center" color="#000" fontWeight="500">
+            {!hideNumbers && "TL"}
+          </Text>
+        </Flex>
+        <PieChart
+          series={[
+            {
+              paddingAngle: 5,
+              innerRadius: 60,
+              outerRadius: 80,
+              data: isValueZero ? newData : data,
+            },
+          ]}
+          width={400}
+          height={200}
+          slotProps={{
+            legend: { hidden: true },
+          }}
+          colors={isValueZero ? ["#E0E0E0"] : backgroundColor}
+          tooltip={{}}
+        />
+
+        <Flex
+          position="absolute"
+          right="50px"
+          top="20px"
+          flexDirection="column"
+        >
+          <Flex gap="5px" justifyContent="center" alignItems="center">
+            <Flex
+              backgroundColor={backgroundColor[0]}
+              width="16px"
+              height="16px"
+              borderRadius="5px"
+            ></Flex>
+            <Text>{chartLabels}</Text>
+          </Flex>
+          <Flex width="100%" alignItems="center" justifyContent="center">
+            <Text fontWeight="700" color="#000" fontSize="14px">
+              {!hideNumbers && `${totalValue} TL`}
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
--- pages/dashboard/components/ChartTab.tsx
+++ pages/dashboard/components/ChartTab.tsx
@@ -1,38 +1,80 @@
 import React from "react";
-import { Flex, Text } from "@/styles/styles";
+import { Flex, Text, Button } from "@/styles/styles";
 import Chart from "./Chart";
 import useUser from "@/hooks/useGetUser";
 import useCurrency from "../hooks/useCurrency";
+import { usePage } from "../context/PageContext";
 
 const ChartTab: React.FC = () => {
   const user = useUser();
   const currency = useCurrency(user);
+  const { handleHideNumbers, hideNumbers } = usePage();
 
-  console.log(typeof currency);
   const data = [
     {
       label: "Vadesiz Hesap",
       value: currency,
     },
   ];
-  const backgroundColor = ["#49A5E0"];
+
+  const cardData = [
+    {
+      label: "Kredi Kartı",
+      value: 0,
+    },
+  ];
 
   return (
-    <Flex flexDirection="column" width="100%">
+    <Flex
+      flexDirection="column"
+      width="100%"
+      padding="15px"
+      position="relative"
+    >
       <Text fontWeight="700" color="#1c345c">
         Finansal Durumum
       </Text>
       <Flex>
         <Chart
           labels={data.map((item) => item.label)}
-          backgroundColor={backgroundColor}
+          backgroundColor={["#49A5E0"]}
           data={data}
+          chartText="Varlıklarım"
         />
         <Chart
           labels={data.map((item) => item.label)}
-          backgroundColor={backgroundColor}
-          data={data}
+          backgroundColor={["#B23508"]}
+          chartText="Borçlarım"
+          data={cardData}
         />
+      </Flex>
+
+      <Flex
+        width="100%"
+        justifyContent="space-between"
+        alignItems="center"
+        borderBottom="1px solid #e5e5e5 "
+        padding="10px 5px"
+      >
+        <Text
+          textDecoration="underline"
+          cursor="pointer"
+          color="#69a6e1"
+          onClick={() => {
+            handleHideNumbers();
+          }}
+        >
+          Sayıları {hideNumbers ? "Göster" : "Gizle"}
+        </Text>
+        <Button
+          border="1px solid #C1C9D3"
+          backgroundColor="transparent"
+          padding="0.6rem 1rem"
+        >
+          <Text color="#69a6e1" cursor="pointer" textAlign="center">
+            Finansal Durum Detayı
+          </Text>
+        </Button>
       </Flex>
     </Flex>
   );
