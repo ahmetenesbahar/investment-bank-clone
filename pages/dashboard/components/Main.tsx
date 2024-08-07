d0fdef97f519d057257aabe1da68d2d19b566508
ahmetenesbahar <enesbahar2303@gmail.com>
2024-08-07 12:35:28 +0300

    refactor: remove unused useMediaQuery hook in Main component

--- pages/dashboard/components/Main.tsx
+++ pages/dashboard/components/Main.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import DashboardLeft from "./DashboardLeft";
 import DashboardRight from "./DashboardRight";
 import { DashboardMainContainer } from "@/styles/styles";
-import useMediaQuery from "@/hooks/useMediaQuery";
 
 const Main: React.FC = () => {
-  const width = useMediaQuery();
   return (
     <DashboardMainContainer backgroundColor="#fff" margin="40px 20px 40px 10px">
       <DashboardLeft />
