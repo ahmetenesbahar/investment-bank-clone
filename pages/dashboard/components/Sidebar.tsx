335c5850f74ec856f441110da1a5ac0ee5ec6938
ahmetenesbahar <enesbahar2303@gmail.com>
2024-08-07 10:13:55 +0300

    refactor: update Sidebar component height for responsive design

--- pages/dashboard/components/Sidebar.tsx
+++ pages/dashboard/components/Sidebar.tsx
@@ -312,7 +312,7 @@ const Sidebar: React.FC = () => {
 
   return (
     <DashboardSidebarContainer
-      height="calc(100vh - 44px)"
+      height=" 100vh "
       backgroundColor={width <= 1280 ? "#1C345C" : "#08335e"}
       flexDirection="column"
       zIndex="2"
