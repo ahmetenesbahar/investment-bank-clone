8baa960c10e6b2aa3ca2f8b7229504f44433fffe
ahmetenesbahar <enesbahar2303@gmail.com>
2024-08-12 18:57:29 +0300

    chore: Add QueryClientProvider and context providers for Date and Modal in Dashboard page

--- pages/dashboard/index.page.tsx
+++ pages/dashboard/index.page.tsx
@@ -9,25 +9,36 @@ import { Container, Flex } from "@/styles/styles";
 
 import { TabProvider } from "./context/TabContext";
 import { PageProvider } from "./context/PageContext";
+import { DateProvider } from "./context/DateContext";
+import { ModalProvider } from "./context/ModalContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { GetStaticProps } from "next";
 
 import { colors } from "@/styles/colors";
 
+const queryClient = new QueryClient();
+
 const Dashboard: NextPage = () => {
   return (
-    <PageProvider>
-      <TabProvider>
-        <Container backgroundColor={colors.secondaryWhite}>
-          <Navbar />
-          <Flex>
-            <Sidebar />
-            <Main />
-          </Flex>
-        </Container>
-      </TabProvider>
-    </PageProvider>
+    <QueryClientProvider client={queryClient}>
+      <DateProvider>
+        <ModalProvider>
+          <PageProvider>
+            <TabProvider>
+              <Container backgroundColor={colors.secondaryWhite}>
+                <Navbar />
+                <Flex>
+                  <Sidebar />
+                  <Main />
+                </Flex>
+              </Container>
+            </TabProvider>
+          </PageProvider>
+        </ModalProvider>
+      </DateProvider>
+    </QueryClientProvider>
   );
 };
 
