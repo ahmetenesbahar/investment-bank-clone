d4e6722cf1b50d31ac961c150623688293456f38
ahmetenesbahar <enesbahar2303@gmail.com>
2024-08-13 11:23:24 +0300

    refactor: refactor components to improve organization and reusability

--- pages/dashboard/index.page.tsx
+++ pages/dashboard/index.page.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import { NextPage } from "next";
 
-import Sidebar from "../../components/Sidebar";
 import Main from "./components/Main";
-import Navbar from "../../components/Navbar";
 import Layout from "@/components/Layout";
-
-import { Container, Flex } from "@/styles/styles";
 
 import { TabProvider } from "./context/TabContext";
 import { PageProvider } from "./context/PageContext";
 import { DateProvider } from "./context/DateContext";
 import { ModalProvider } from "./context/ModalContext";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { GetStaticProps } from "next";
-
-import { colors } from "@/styles/colors";
-
-const queryClient = new QueryClient();
 
 const Dashboard: NextPage = () => {
   return (
