82d57e765e354e87571e44292e79ab9b6504ba61
ahmetenesbahar <enesbahar2303@gmail.com>
2024-08-14 16:43:48 +0300

    refactor: add Content component to AccountInfo page

--- pages/my_accounts/account_info/index.page.tsx
+++ pages/my_accounts/account_info/index.page.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { NextPage } from "next";
 
 import Layout from "@/components/Layout";
+import Content from "./components/Content";
 
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { GetStaticProps } from "next";
 
 const AccountInfo: NextPage = () => {
-  return <Layout>asd</Layout>;
+  return (
+    <Layout>
+      <Content />
+    </Layout>
+  );
 };
 
 export default AccountInfo;
