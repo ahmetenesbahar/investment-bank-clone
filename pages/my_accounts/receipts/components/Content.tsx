ab73d9ac2193ffad702e61162086de156dd5d561
ahmetenesbahar <enesbahar2303@gmail.com>
2024-08-21 15:58:10 +0300

    feat: Add Table, InfoBox, and SelectedReceipts components to receipts page

--- pages/my_accounts/receipts/components/Content.tsx
+++ pages/my_accounts/receipts/components/Content.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { NextPage } from "next";
 import { ContentContainer } from "@/styles/styles";
+import { Flex } from "@/styles/styles";
 
 import Header from "./Header/Header";
 import Filter from "./Filter/Filter";
+import Table from "./Table/Table";
+import InfoBox from "./InfoBox/InfoBox";
+import SelectedReceipts from "./SelectedReceipts/SelectedReceipts";
+import { useItem } from "../context/ItemContext";
 
 const Content: NextPage = () => {
+  const { checkedItems } = useItem();
   return (
-    <ContentContainer flexDirection="column" padding="1.25rem" width="100%">
-      <Header />
-      <Filter />
+    <ContentContainer flexDirection="column" width="100%">
+      <Flex flexDirection="column" padding="1.25rem">
+        <Header />
+        <Filter />
+        <InfoBox />
+        <Table />
+      </Flex>
+      {checkedItems.size > 0 && <SelectedReceipts />}
     </ContentContainer>
   );
 };
